f88858f4e5846ba1bca00c57c4dac68d1966978b
anushka patil <87687044+aanushkee@users.noreply.github.com>
2023-04-26 18:25:49 +0545

    Update preorder.c

--- preorder.c
+++ preorder.c
@@ -1,12 +1,20 @@
-// Pre-order traversal function to visit all nodes in the R-tree
-
-void preOrderTraversal(struct RTreeNode * node, void (*callback)(struct RTreeNode *)) 
-{
-  if (node == NULL) {
-    return;
-  }
-  callback(node);
-  for (int i = 0; i < node->numChildren; i++) {
-    preOrderTraversal(node->child_pointer[i], callback);
-  }
+void preOrderTraversal(struct RTreeNode* node) {
+    if (node == NULL) {
+        return;
+    }
+    if (node->numChildren == 0) {
+        // The current node is a leaf node, print all the objects in it.
+        for (int i = 0; i < MAX_CHILDREN; i++) {
+            if (node->objects[i].x != 0 && node->objects[i].y != 0) {
+                printf("(%d,%d) ", node->objects[i].x, node->objects[i].y);
+            }
+        }
+        return;
+    }
+    // The current node is an internal node, recursively traverse each of its child nodes.
+    for (int i = 0; i < MAX_CHILDREN; i++) {
+        if (node->child_pointer[i] != NULL) {
+            preOrderTraversal(node->child_pointer[i]);
+        }
+    }
 }
