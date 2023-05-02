6e92c62f4dc10e46d141a847c70ab34881a645f2
anushka patil <87687044+aanushkee@users.noreply.github.com>
2023-05-02 14:27:19 +0545

    changes

--- search.c
+++ search.c
@@ -1,29 +1,22 @@
 #include "dsa_project.h"
 
-void search(struct RTree *node, DataPoint D)
-{
-    // If node is not a leaf, check each entry E to see if E.I overlaps D
-    if (node->numChildren > 0)
-    {
-        for (int i = 0; i < node->numChildren; i++)
-        {
-            if (calculateOverlap(node->child_pointer[i]->mbr, D.rect) > 0)
-            {
-                // If there is overlap, start a recursive search on the child node
+void search(RTreeNode *node, DataPoint D) {
+
+    if (node->isLeaf) { // S2 [Search leaf node]
+
+        for (int i = 0; i < node->numObjects; i++) {
+            if (calculateOverlap(node->objects[i].mbr, D.mbr)) {
+                printf("Area: %d\n", calculatePointArea(node->objects[i].points, node->objects[i].num_points));
+            }
+        }
+
+    } else { // S1 [Search Subtree]
+
+        for (int i = 0; i < node->numChildren; i++) {
+            if (calculateOverlap(node->child_pointer[i]->mbr, D.mbr)) {
                 search(node->child_pointer[i], D);
             }
         }
-    }
-    else
-    {
-        // If node is a leaf, check each entry E to see if E.I overlaps D
-        for (int i = 0; i < node->numObjects; i++)
-        {
-            if (calculateOverlap(node->objects[i].rect, D.rect) > 0)
-            {
-                // If there is overlap, print the area of the DataPoint
-                printf("Area: %d\n", calculatePointArea(&D, 1));
-            }
-        }
+
     }
 }
