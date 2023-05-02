431788521cbd0b4a280e220c7b39da38ed814853
anushka patil <87687044+aanushkee@users.noreply.github.com>
2023-05-02 14:29:20 +0545

    final changes

--- search.c
+++ search.c
@@ -1,22 +1,31 @@
 #include "dsa_project.h"
 
-void search(RTreeNode *node, DataPoint D) {
-
-    if (node->isLeaf) { // S2 [Search leaf node]
-
-        for (int i = 0; i < node->numObjects; i++) {
-            if (calculateOverlap(node->objects[i].mbr, D.mbr)) {
-                printf("Area: %d\n", calculatePointArea(node->objects[i].points, node->objects[i].num_points));
+void search(RTreeNode* node, DataPoint D)
+{
+    if (node->isLeaf)
+    {
+        // Check each entry in the leaf node
+        for (int i = 0; i < node->numObjects; i++)
+        {
+            // Check for overlap
+            if (calculateOverlap(node->objects[i].mbr, D.mbr) > 0)
+            {
+                printf("Object area: %d\n", calculatePointArea(node->objects[i].points, node->objects[i].numPoints));
             }
         }
-
-    } else { // S1 [Search Subtree]
-
-        for (int i = 0; i < node->numChildren; i++) {
-            if (calculateOverlap(node->child_pointer[i]->mbr, D.mbr)) {
+    }
+    else
+    {
+        // Check each entry in the non-leaf node
+        for (int i = 0; i < node->numChildren; i++)
+        {
+            // Check for overlap
+            if (calculateOverlap(node->child_pointer[i]->mbr, D.mbr) > 0)
+            {
+                // Recurse on the child node
                 search(node->child_pointer[i], D);
             }
         }
-
     }
 }
+
