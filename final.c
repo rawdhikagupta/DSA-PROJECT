9dddb813f619e2cb70494901649d476e12612761
dhawal-mehta-2307 <118529036+dhawal-mehta-2307@users.noreply.github.com>
2023-05-02 19:03:55 +0530

    Update final.c

--- final.c
+++ final.c
@@ -601,34 +601,41 @@ void insertDataPoint(DataPoint* point, RTree* tree) {
     adjustTree(leaf);
 }
 
-void printTree(RTreeNode* node) {
-    if (node == NULL)
-    {
-        return;
-    }
+void printRTree(struct RTreeNode *node)
+{
+  if (node == NULL)
+  {
+    return;
+  }
 
-    if (node->numChildren == 0)
-    { // Leaf node
-        for (int i = 0; i < node->numObjects; i++)
-        {
-        printf("(%ld, %ld) ", node->objects[i]->x, node->objects[i]->y);
-        }
-        printf("\n");
+  if (node->numChildren == 0)
+  { // Leaf node
+    printf("Leaf Node - Objects: ");
+    for (int i = 0; i < node->numObjects; i++)
+    {
+      printf("(%d, %d) ", node->objects[i].x, node->objects[i].y);
     }
-    else
-    { // Internal node
-    if(node->parent!=NULL) {
-        for (int i = 0; i < node->numChildren; i++)
-        {
-            printTree(node->children[i]);
-        }
-    } else if(node->parent == NULL) {
-        for (int i = 0; i < node->numChildren; i++)
-        {
-        printTree(node->children[i]);
-        }
-        }
+    printf("\n");
+  }
+  else
+  { // Internal node
+  if(node->parent!=NULL)
+    {
+      printf("\nInternal Node - MBR: {(%d, %d), (%d, %d)}\n", node->mbr.xmin, node->mbr.ymin, node->mbr.xmax, node->mbr.ymax);
+      for (int i = 0; i < node->numChildren; i++)
+      {
+        printRTree(node->child_pointer[i]);
+      }
+   }
+    else if(node->parent == NULL)
+    {
+      printf("RootNode - MBR: {(%d, %d), (%d, %d)}\n", node->mbr.xmin, node->mbr.ymin, node->mbr.xmax, node->mbr.ymax);
+    for (int i = 0; i < node->numChildren; i++)
+    {
+      printRTree(node->child_pointer[i]);
     }
+    }
+  }
 }
 
 void freeNodes(RTreeNode* node) {
